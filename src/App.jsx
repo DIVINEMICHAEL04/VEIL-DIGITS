492ac443a09d1072ea2ccaed3d53ee6a7b028e84
mega-ifiok001 <ifiokaniebiet@gmail.com>
2025-06-11 23:20:12 +0100

    import init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 // import Header from './components/header/header.jsx'
-import Home from './pages/Home';
+import Home from '../src/pages/Home/Home';
 
 function App() {
 
